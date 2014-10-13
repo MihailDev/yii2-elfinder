cf5d666f7efcd0e1a7fffb8fbffc892d29899edc
Sergei Gulin <gulin-sergei@ya.ru>
2014-10-13 11:03:40 +0400

    Update InputFile.php
    en:
    need to dynamically create a widget
    ru:
    необходимо для динамического создания виджета

--- InputFile.php
+++ InputFile.php
@@ -82,8 +82,9 @@ class InputFile extends InputWidget{
         AssetsCallBack::register($this->getView());
 
         if (!empty($this->multiple))
-            $this->getView()->registerJs("ElFinderFileCallback.register(".Json::encode($this->options['id']).", function(files, id){ var _f = []; for (var i in files) { _f.push(files[i].url); } \$('#' + id).val(_f.join(', ')); return true;}); $('#".$this->buttonOptions['id']."').click(function(){ElFinderFileCallback.openManager(".Json::encode($this->_managerOptions).");});");
+            $this->getView()->registerJs("ElFinderFileCallback.register(" . Json::encode($this->options['id']) . ", function(files, id){ var _f = []; for (var i in files) { _f.push(files[i].url); } \$('#' + id).val(_f.join(', ')); return true;}); $(document).on('click','#" . $this->buttonOptions['id'] . "', function(){ElFinderFileCallback.openManager(" . Json::encode($this->_managerOptions) . ");});");
         else
-            $this->getView()->registerJs("ElFinderFileCallback.register(".Json::encode($this->options['id']).", function(file, id){ \$('#' + id).val(file.url); return true;}); $('#".$this->buttonOptions['id']."').click(function(){ElFinderFileCallback.openManager(".Json::encode($this->_managerOptions).");});");
+            $this->getView()->registerJs("ElFinderFileCallback.register(" . Json::encode($this->options['id']) . ", function(file, id){ \$('#' + id).val(file.url); return true;}); $(document).on('click', '#" . $this->buttonOptions['id'] . "', function(){ElFinderFileCallback.openManager(" . Json::encode($this->_managerOptions) . ");});");
+
     }
 }
