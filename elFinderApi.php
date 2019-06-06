e0b6f81518e4a472256b80ae79b4a738c5b5df59
Dzmitry Saniuk <32801067+scorpsan@users.noreply.github.com>
2019-06-06 17:24:51 +0300

    Update elFinderApi.php

--- elFinderApi.php
+++ elFinderApi.php
@@ -22,7 +22,10 @@ class elFinderApi extends \elFinder
             foreach ($plugin->bind as $cmd => $methods) {
                 $doRegist = (strpos($cmd, '*') !== false);
                 if (! $doRegist) {
-                    $_getcmd = create_function('$cmd', 'list($ret) = explode(\'.\', $cmd);return trim($ret);');
+                    //$_getcmd = create_function('$cmd', 'list($ret) = explode(\'.\', $cmd);return trim($ret);');
+                    $_getcmd = function ($cmd) {
+                        list($ret) = explode('.', $cmd);return trim($ret);
+                    };
                     $doRegist = ($_reqCmd && in_array($_reqCmd, array_map($_getcmd, explode(' ', $cmd))));
                 }
                 if ($doRegist) {
